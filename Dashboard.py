7afa7e162b8e415591107af6fde282dec3b6bc9e
seakr1948 <48813264+seakr1948@users.noreply.github.com>
2021-03-17 14:46:29 -0700

    Added an alert Label that shows which items are low in stock

--- Dashboard.py
+++ Dashboard.py
@@ -16,9 +16,6 @@ for r in records:
     product_labels.append(r[0])
     prices.append(r[1])
 
-size_of_groups=[12,11,3,30]
-names=['groupA', 'groupB', 'groupC', 'groupD']
-
 fig = Figure()
 ax = fig.add_subplot(111)
 ax.set(title='Inventory')
@@ -27,12 +24,29 @@ ax.pie(prices, radius=1, labels=product_labels, shadow=True)
 circle=matplotlib.patches.Circle((0,0), 0.7, color='white')
 ax.add_artist(circle)
 
+c.execute("SELECT product_name, quantity FROM products WHERE quantity <= 5;")
+records = c.fetchall()
+if records == None:
+    LowAlertString = "No alerts at the moment"
+else:
+    LowAlertString = "These items are low in stock: \n"
+    temp = ""
+    for r in records:
+        temp += ('{:40s} {:4.1f} \n').format(r[0], r[1])
+    print(temp)
+    LowAlertString += temp
+
 def Dashboard(Tab):
     
+    labelframe = LabelFrame(Tab, text="Alerts")
+    labelframe.grid(column=1, row=0)
+    AlertLabels = Label(labelframe, text=LowAlertString)
+    AlertLabels.pack()
+
+
     option_frame = ttk.Frame(Tab)
     option_frame.grid(row=5, column=7, padx=20, pady=(10, 0))
-    w = Button(option_frame, text="Add Inventory Item")
-    w.grid(row=9, column=9)
+    
 
     chart1 = FigureCanvasTkAgg(fig,Tab)
-    chart1.get_tk_widget().grid()
+    chart1.get_tk_widget().grid(row=0,column=0)
