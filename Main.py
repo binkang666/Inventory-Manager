031d2604eb7da6587e767d14a015b3f2c4b3578f
Chiaki-Shen <55766882+Chiaki-Shen@users.noreply.github.com>
2021-03-18 14:54:35 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,28 +1,27 @@
-from New_UI import NewerVersion
+from tkinter import *
+from tkinter import ttk
+import tkinter as tk
+import sqlite3
 from Dashboard import Dashboard
-from sqlite3.dbapi2 import DatabaseError
-from Inventory import InventoryTab
+from frontEnd_V0_3 import MainScreen
 
-import tkinter as tk                     
-from tkinter import ttk 
-  
-  
-root = tk.Tk() 
-root.geometry("900x600")
-root.title("Tab Widget") 
-tabControl = ttk.Notebook(root) 
-  
-tab1 = ttk.Frame(tabControl) 
-tab2 = ttk.Frame(tabControl) 
-tab3 = ttk.Frame(tabControl)
-  
-tabControl.add(tab1, text ='Inventory') 
-tabControl.add(tab2, text ='Dashboard') 
-tabControl.add(tab3, text = 'Invent_V2')
-tabControl.grid(row= 0, column=0) 
-  
-InventoryTab(tab1) 
+
+root = tk.Tk()
+root.title('CECS 445 Inventory Manager By Team Boba')
+
+ws = root.winfo_screenwidth()
+hs = root.winfo_screenheight()
+x = (ws / 2) - (1400 / 2)
+y = (hs / 2) - (700 / 2)
+root.geometry('%dx%d+%d+%d' % (1400, 700, x, y))
+tabControl = ttk.Notebook(root)
+tab1 = ttk.Frame(tabControl)
+tab2 = ttk.Frame(tabControl)
+
+tabControl.add(tab1, text="Main Tab")
+tabControl.add(tab2, text="Dashboard")
+tabControl.grid(row=0, column=0)
+MainScreen(tab1,root)
 Dashboard(tab2)
-NewerVersion(tab3)
-
 root.mainloop()
+
